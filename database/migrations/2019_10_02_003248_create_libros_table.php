8de3e3f2958a5d54e5178d1dbdca3fb21e71ac47
Jcpzo97 <Jcpzo97@gmail.com>
2019-10-01 20:22:29 -0500

    Avance 2

---
+++ database/migrations/2019_10_02_003248_create_libros_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('libros', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('titulo');
+            $table->mediumText('descripcion');
+            $table->text('contenido');
+            $table->timestamp('fecha');
+            $table->unsignedInteger('categoria_id');//categoria id
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('libros');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Libro;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +9,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    $libros = Libro::all();
+    return view('welcome', compact('libros'));
+});
